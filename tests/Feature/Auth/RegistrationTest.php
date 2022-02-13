10c707571cb07246b6e706b42549d9c76f1f77fa
CubeStorm <cubestorm72@gmail.com>
2022-02-13 23:46:58 +0100

    Updated default tests

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -11,12 +11,7 @@ class RegistrationTest extends TestCase
 
     public function test_new_users_can_register()
     {
-        $response = $this->post('/register', [
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 'password',
-            'password_confirmation' => 'password',
-        ]);
+        $response = $this->post('/register');
 
         $this->assertAuthenticated();
         $response->assertNoContent();
