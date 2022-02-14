e16463a233b52aaff4e7f2d6aa446350243b2b9e
CubeStorm <cubestorm72@gmail.com>
2022-02-14 19:05:31 +0100

    Users api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::middleware('auth:sanctum')->get('/user', fn (Request $request) => $request->user());
+
+Route::get('/users', UserController::class);
