f65c3b4e6d75d8dd7bd7338621493de948f4460c
CubeStorm <cubestorm72@gmail.com>
2022-02-22 17:01:22 +0100

    More friends and users for tests

--- database/seeders/TestsSeeder.php
+++ database/seeders/TestsSeeder.php
@@ -17,13 +17,70 @@ class TestsSeeder extends Seeder
         User::truncate();
         Friendship::truncate();
 
-        User::create([
+        $testUser = User::create([
             'first_name' => 'Jane',
             'last_name' => 'Doe',
             'email' => 'test@test.com',
             'password' => Hash::make('test'),
             'profile_image' => $faker->imageUrl(168, 168),
             'background_image' => $faker->imageUrl(850, 350)
+        ]);
+
+        $friendUser = User::create([
+            'first_name' => 'Friend',
+            'last_name' => 'User',
+            'email' => 'friend@test.com',
+            'password' => Hash::make('test'),
+            'profile_image' => $faker->imageUrl(168, 168),
+            'background_image' => $faker->imageUrl(850, 350)
+        ]);
+
+        Friendship::create([
+            'first_user' => $testUser->id,
+            'second_user' => $friendUser->id,
+            'acted_user' => $testUser->id,
+            'status' => 'confirmed'
+        ]);
+
+        User::create([
+            'first_name' => 'UserTo',
+            'last_name' => 'Invite',
+            'email' => 'invite@test.com',
+            'password' => Hash::make('test'),
+            'profile_image' => $faker->imageUrl(168, 168),
+            'background_image' => $faker->imageUrl(850, 350)
+        ]);
+
+        $userToAccept = User::create([
+            'first_name' => 'UserTo',
+            'last_name' => 'Accept',
+            'email' => 'accept@test.com',
+            'password' => Hash::make('test'),
+            'profile_image' => $faker->imageUrl(168, 168),
+            'background_image' => $faker->imageUrl(850, 350)
+        ]);
+
+        Friendship::create([
+            'first_user' => $userToAccept->id,
+            'second_user' => $testUser->id,
+            'acted_user' => $userToAccept->id,
+            'status' => 'pending'
+        ]);
+
+        $userToReject = User::create([
+            'first_name' => 'UserTo',
+            'last_name' => 'Reject',
+            'email' => 'reject@test.com',
+            'password' => Hash::make('test'),
+            'profile_image' => $faker->imageUrl(168, 168),
+            'background_image' => $faker->imageUrl(850, 350)
+        ]);
+
+        Friendship::create([
+            'first_user' => $userToReject->id,
+            'second_user' => $testUser->id,
+            'acted_user' => $userToReject->id,
+            'status' => 'pending'
         ]);
     }
 }
