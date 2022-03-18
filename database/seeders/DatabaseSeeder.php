503a76a90dd19ae515ecf863631c7a7e5ab75b3e
CubeStormPrivate <marczelloweb@gmail.com>
2022-03-18 18:33:17 +0100

    Clearing database before refreshing seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,12 +2,23 @@
 
 namespace Database\Seeders;
 
+use App\Models\Friendship;
+use App\Models\Message;
+use App\Models\Poke;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
+        User::truncate();
+        Friendship::truncate();
+        Poke::truncate();
+        Message::truncate();
+        DB::table('notifications')->truncate();
+
         $this->call([
             UserSeeder::class,
             FriendshipSeeder::class,
