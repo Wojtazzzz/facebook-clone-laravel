9023972d0b65e581d38bb770d3093a823b040bac
CubeStorm <cubestorm72@gmail.com>
2022-07-21 15:27:24 +0200

    Missing SavedPostFactory

--- database/factories/SavedPostFactory.php
+++ database/factories/SavedPostFactory.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Database\Factories;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class SavedPostFactory extends Factory
@@ -11,7 +13,8 @@ class SavedPostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'post_id' => Post::factory(),
         ];
     }
 }
