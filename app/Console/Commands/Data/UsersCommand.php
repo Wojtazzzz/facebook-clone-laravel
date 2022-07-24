46c4eea45e791423ccd9fab27cb7667649316d74
CubeStorm <cubestorm72@gmail.com>
2022-07-24 15:07:25 +0200

    Command for creating mock users

---
+++ app/Console/Commands/Data/UsersCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands\data;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class UsersCommand extends Command
+{
+    protected $signature = 'data:users {amount=1}';
+    protected $description = 'Create specific amount of users';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle(): void
+    {
+        if (!$this->checkAmount()) {
+            return;
+        }
+
+        $amount = $this->argument('amount');
+
+        User::factory($amount)->create();
+
+        $this->info('User(s) created successfully.');
+    }
+
+    private function checkAmount(): bool
+    {
+        if ((int) $this->argument('amount') >= 1) {
+            return true;
+        }
+
+        $this->error('Amount must be integer greater than 0.');
+
+        return false;
+    }
+}
---
+++ tests/Feature/Console/Data/UsersCommandTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Console\Data;
+
+use Tests\TestCase;
+
+class UsersCommandTest extends TestCase
+{
+    private string $command = 'data:users';
+    private string $table = 'users';
+
+    public function testExecuteWithSuccess(): void
+    {
+        $this->artisan($this->command)
+            ->assertSuccessful();
+    }
+
+    public function testCreateSpecificAmountOfUsers(): void
+    {
+        $this->artisan($this->command.' 7');
+
+        $this->assertDatabaseCount($this->table, 7);
+    }
+
+    public function testPrintProperlyMessageWhenSuccess(): void
+    {
+        $this->artisan($this->command)
+            ->expectsOutput('User(s) created successfully.');
+    }
+
+    public function testCannotPassStringAsAmountArgument(): void
+    {
+        $this->artisan($this->command.' ugly_string')
+            ->expectsOutput('Amount must be integer greater than 0.')
+            ->doesntExpectOutput('User(s) created successfully.');
+    }
+
+    public function testCreateOnlyOneUserWhenAmountNotPassed(): void
+    {
+        $this->artisan($this->command)
+            ->expectsOutput('User(s) created successfully.');
+
+        $this->assertDatabaseCount($this->table, 1);
+    }
+}
