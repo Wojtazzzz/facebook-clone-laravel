d11eed4081b319a86da61c40ede22786ab34853d
CubeStorm <cubestorm72@gmail.com>
2022-08-23 18:44:55 +0200

    bug: read_at could be null

--- app/Http/Resources/MessageResource.php
+++ app/Http/Resources/MessageResource.php
@@ -14,7 +14,7 @@ class MessageResource extends JsonResource
             'text' => $this->text,
             'is_received' => $this->sender_id !== Auth::user()->id,
             'status' => $this->status,
-            'read_at' => $this->read_at->dependentFormat(),
+            'read_at' => $this->read_at && $this->read_at->dependentFormat(),
             'created_at' => $this->created_at->dependentFormat(),
         ];
     }
