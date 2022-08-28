dbd2b0298ab664d5d0a457f4dc41ba21649cf8aa
CubeStorm <cubestorm72@gmail.com>
2022-08-28 09:00:05 +0200

    build: remove algolia legacy

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,19 +3,15 @@
 namespace App\Providers;
 
 use App\Models\Post;
-use App\Models\User;
 use App\Observers\PostObserver;
-use App\Observers\UserObserver;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
 {
-    protected $listen = [
-    ];
+    protected $listen = [];
 
     protected $observers = [
         Post::class => [PostObserver::class],
-        User::class => [UserObserver::class],
     ];
 
     public function boot()
