191256c0c8c1869c11f9138d81aae93b82106cc7
CubeStorm <cubestorm72@gmail.com>
2022-09-29 21:32:45 +0200

    test: change sqlite db to mysql for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-    use RefreshDatabase;
     use WithFaker;
 
     public function setUp(): void
@@ -19,5 +18,7 @@ abstract class TestCase extends BaseTestCase
         parent::setUp();
 
         $this->setUpFaker();
+
+        Artisan::call('data:clear');
     }
 }
