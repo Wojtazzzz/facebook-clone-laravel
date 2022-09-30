6b497016081eef98e730ea41b9aaff887ab5d0ad
CubeStorm <cubestorm72@gmail.com>
2022-09-30 10:15:35 +0200

    test: add missing RefreshDatabase trait to TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Artisan;
@@ -11,6 +12,7 @@ use Illuminate\Support\Facades\Artisan;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use RefreshDatabase;
     use WithFaker;
 
     public function setUp(): void
@@ -18,7 +20,5 @@ abstract class TestCase extends BaseTestCase
         parent::setUp();
 
         $this->setUpFaker();
-
-        Artisan::call('data:clear');
     }
 }
