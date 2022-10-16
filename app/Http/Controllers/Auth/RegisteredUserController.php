81c2b161bd8f464bddb344297f70045b907c3503
CubeStorm <cubestorm72@gmail.com>
2022-10-16 18:26:50 +0200

    fix: register process generate additional user info

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -6,27 +6,14 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Faker\Generator;
-use Illuminate\Container\Container;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 
 class RegisteredUserController extends Controller
 {
     public function store(): Response
     {
-        $faker = Container::getInstance()->make(Generator::class);
-
-        $user = User::create([
-            'first_name' => $faker->firstName,
-            'last_name' => $faker->lastName,
-            'email' => $faker->email,
-            'password' => Hash::make($faker->iban),
-            'profile_image' => $faker->picsumStaticRandomUrl(168, 168),
-            'background_image' => $faker->picsumStaticRandomUrl(850, 350),
-            'born_at' => now(),
-        ]);
+        $user = User::factory()->create();
 
         Auth::login($user);
 
